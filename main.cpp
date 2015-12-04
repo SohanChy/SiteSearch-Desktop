55aef6770c0a3f9b2169ab79df5005a8ae268da6
Sohan Chowdhury <sifat3d@gmail.com>
2015-12-04 20:34:08 +0600

    Test and use newly made functions and implement search of links inside base link

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstring>
+#include <cstdio>
+
 #include "prototypes.h"
 
 using namespace std;
+
+bool checkIfVisited(vector<string> *linkDatabase, string link)
+{
+        for(vector<string>::const_iterator i = linkDatabase->begin(); i != linkDatabase->end(); i++)
+        {
+            string tmpLink = *i;
+            if(tmpLink == link)
+                {
+                    return true;
+                }
+        }
+        return false;
+}
 
 void printVectorString(vector<string> *vectorString) {
 
@@ -15,11 +31,50 @@ void printVectorString(vector<string> *vectorString) {
 
 int main()
 {
-    char siteUrl[1000]="google.com";
+    freopen("output.txt","w",stdout);
 
-    vector<string> siteLinks = getLinks(pageString(siteUrl),siteUrl);
+    char siteUrl[1000]="http://103.3.226.207";
+    string keyword = "Sohan";
+
+    string siteAsString = pageString(siteUrl);
+    vector<string> siteLinks = getLinks(&siteAsString,siteUrl);
+
+
+    vector<string> visitedLinks;
 
     printVectorString(&siteLinks);
+
+
+    for(vector<string>::const_iterator i = siteLinks.begin(); i != siteLinks.end(); i++)
+        {
+            if(checkIfVisited(&visitedLinks, *i) == false)
+                {
+                char * tmpThisSiteUrl = new char [(i->length())+1];
+                strcpy (tmpThisSiteUrl, i->c_str());
+                cout<<"Fetching url "<<tmpThisSiteUrl<<" @@@"<<endl;
+
+                string tmpSiteHtml = pageString(tmpThisSiteUrl);
+
+
+                vector<string> thisPagesLinks = getLinks(&tmpSiteHtml,siteUrl);
+                visitedLinks.push_back(*i);
+
+                cout<<"---------------------------------"<<endl;
+                printVectorString(&thisPagesLinks);
+                cout<<"---------------------------------"<<endl;
+                }
+        }
+
+
+
+    if(searchPageStringForKeyword(&siteAsString,keyword))
+    {
+        cout<<endl<<"FOUND"<<endl;
+    }
+    else
+    {
+        cout<<endl<<"NOT FOUND"<<endl;
+    }
 
 
 
